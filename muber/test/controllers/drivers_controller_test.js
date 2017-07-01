7ac46e576adac04718f9b061c61887948561ebd7
dtroxx <dtroxx@gmail.com>
2017-07-01 14:26:05 -0700

    delete driver completed

--- muber/test/controllers/drivers_controller_test.js
+++ muber/test/controllers/drivers_controller_test.js
@@ -36,4 +36,35 @@ describe('Drivers controller', () => {
         });
     });
   });
+
+  it('Delete to /api/drivers/:id can delete a record', done => {
+    const driver = new Driver({ email: 'test@test.com' });
+
+    driver.save().then(() => {
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.findOne({ email: 'test@test.com'})
+            .then(driver => {
+              assert(driver === null);
+              done();
+          });
+        });
+    });
+  });
+
+  it('Delete to /api/drivers/:id can delete a record', done => {
+    const driver = new Driver({ email: 'test@test.com' });
+
+    driver.save().then(() => {
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.count().then(count => {
+            assert(count === 0);
+            done();
+          });
+        });
+    });
+  });
 });
